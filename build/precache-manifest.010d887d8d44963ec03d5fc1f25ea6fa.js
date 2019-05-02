b2df2d06f6d94809baacfc2ea1e6c6f0428b6cbb
kirk <kzimmer97@github.com>
2019-05-02 03:40:48 -0400

    initial commit

---
+++ build/precache-manifest.010d887d8d44963ec03d5fc1f25ea6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a6c2c4d59535a76e186b",
+    "url": "/static/js/main.19ff98f4.chunk.js"
+  },
+  {
+    "revision": "5540e8cf8e180b110831",
+    "url": "/static/js/2.a6b4cf36.chunk.js"
+  },
+  {
+    "revision": "a6c2c4d59535a76e186b",
+    "url": "/static/css/main.4b9fda8b.chunk.css"
+  },
+  {
+    "revision": "bd4b72bdd0c53de551e0e52729e58bc8",
+    "url": "/index.html"
+  }
+];
